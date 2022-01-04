6f0cb774c3bdcb7f97bdbf050bcfc37bfc73ae3a
andreialexc30 <andreialexx30@gmail.com>
2022-01-04 15:29:14 +0200

    Restructured hierarchy

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <header class="searchQuery">
-    <!-- App Name -->
+  <section class="searchQuery">
+    <header class="header">
+      <!-- App Name -->
       <h1 class="section-heading">Movidentifier <img src="./assets/film-solid.svg" alt="" class="heading-icon"></h1>
-    <!-- Navigation TBA -->
-    <!-- <nav id="nav"></nav> -->
+      <!-- Navigation TBA -->
+      <!-- <nav id="nav"></nav> -->
+    </header>
     <!-- Search for movies -->
       <p class="section-text">Navigate the web in search of a great movie</p>
       <SearchQuery />
-  </header>
+  </section>
   <Movies />
   <!-- <router-view/> -->
 </template>
