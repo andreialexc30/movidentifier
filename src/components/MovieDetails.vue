a4ab10b787326ad74d2b6cc9b43797b163a46b1c
andreialexc30 <andreialexx30@gmail.com>
2022-01-06 18:41:03 +0200

    Buttons for watch later and add to cart on found movies

--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -3,6 +3,14 @@
             <div class="movie-art">
                 <!-- TODO: Movie picture -->
                 <img src="../assets/temp.jpg" alt="movie" class="movie-art_img">
+                <div class="movie-art_controls">
+                    <button class="movie-art_controls-btn">
+                        <img src="../assets/watchlater.svg" alt="" class="control-btn_icon">
+                    </button>
+                    <button class="movie-art_controls-btn">
+                        <img src="../assets/plus-circle.svg" alt="" class="control-btn_icon">
+                    </button>
+                </div>
             </div>
             <div class="movie-details">
                 <h1 class="movie-details_title">Title of Movie</h1>
