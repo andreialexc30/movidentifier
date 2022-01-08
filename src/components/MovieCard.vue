b368e69978514ab03ce7ee8f365846a77041d174
andreialexc30 <andreialexx30@gmail.com>
2022-01-08 13:45:37 +0200

    Add Cart movie details component

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="movie-in-cart">
+        <div class="movie-in-cart_img">
+            <img src="/" alt="movie">
+        </div>
+        <div class="movie-in-cart_details">
+            <h1 class="movie-in-cart_title">Movie Title</h1>
+            <p class="movie-in-cart_synposis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates quisquam alias, deleniti ratione vero similique eius ex impedit atque officiis eum et repudiandae nulla molestias consectetur tempore recusandae sequi? Modi hic ratione aperiam nulla necessitatibus tenetur soluta aliquid, corrupti exercitationem.</p>
+        </div>
+        <button class="movie-in-cart_removeBtn">remove</button>
+    </div>
+</template>
