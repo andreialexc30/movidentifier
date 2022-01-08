b579a8d3bce3d362cb3304e221a56c49533881f1
andreialexc30 <andreialexx30@gmail.com>
2022-01-08 13:45:04 +0200

    Add Cart component

---
+++ src/components/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="shopping-cart">
+        <h1 class="shopping-cart_title">Rent a Movie</h1>
+        <div class="shopping-cart_details">
+            <MovieCard />
+            <Sidebar />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import MovieCard from './MovieCard.vue';
+    import Sidebar from './Sidebar.vue';
+
+    export default {
+        components: { MovieCard, Sidebar },
+        name: 'Cart',
+    }
+</script>
