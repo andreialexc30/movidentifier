1da3d0001938e1fff2d9d8018aac59ea0b8966b4
andreialexc30 <andreialexx30@gmail.com>
2022-01-11 14:35:57 +0200

    Modified cart layout related to MovieCard and Sidebar components

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="shopping-cart">
-        <h1 class="shopping-cart_title">Rent a Movie</h1>
-        <div class="shopping-cart_details">
-            <MovieCard />
-            <Sidebar />
+        <div class="shopping-cart_wrapper">
+            <h1 class="shopping-cart_title">
+                Rent a Movie
+                <div class="items">
+                    <span class="number-of-items">0</span>
+                    items
+                </div>
+            </h1>
+            <div class="shopping-cart_details">
+                <MovieCard />
+                <MovieCard />
+            </div>
         </div>
+        <Sidebar />
     </div>
 </template>
 
