548db97df738689786383bd24a7189842d2ef227
andreialexc30 <andreialexx30@gmail.com>
2022-01-19 17:45:54 +0200

    Removed unused code

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,29 +1,19 @@
 <template>
     <section class="movieDetails">
-        <h1 class="movieDetails-heading">
-            We found the following movies
-            <img src="../assets/check-green.svg" alt="" class="heading-icon">
-        </h1>
+        <Warnings v-if="foundMovie" />
         <main class="movie-list">
-            <MovieDetails :movieTitle="this.movie.movieTitle"
-                :movieDirector="this.movie.movieDirector"
-                :movieDate="this.movie.movieDate"
-                :movieSynopsis="this.movie.movieSynopsis"
-                :movieGenres="this.movie.movieGenres"
-                :base_url="this.img.base_url"
-                :size="this.img.size"
-                :file_path="this.img.file_path"
-             />
+            <MovieDetails :fetchedMovies="fetchedMovies"/>
         </main>
     </section>
 </template>
 
 <script lang="ts">
     import MovieDetails from './MovieDetails.vue';
+    import Warnings from './Warnings.vue';
 
     export default {
-        props: ['movie', 'img'],
-        components: { MovieDetails },
+        props: ['fetchedMovies', 'movie_id', 'foundMovie'],
+        components: { MovieDetails, Warnings },
         name: 'Movies'
     }
 </script>
