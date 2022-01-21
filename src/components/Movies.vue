082e7512bc4489b3bdcd912fba99f1027d8b10a9
andreialexc30 <andreialexx30@gmail.com>
2022-01-21 14:27:19 +0200

    New h1 with v-else that shows if no movies are found/displayed

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="movieDetails">
         <Warnings v-if="foundMovie" />
+        <h1 class="movieDetails-heading" v-else>No movies to display</h1>
         <main class="movie-list">
             <MovieDetails :fetchedMovies="fetchedMovies"/>
         </main>
