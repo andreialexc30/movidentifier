7f8fadc54e14e7f79371c437c97733b43c689e5d
andreialexc30 <andreialexx30@gmail.com>
2022-01-21 14:27:44 +0200

    Added key binding to movieObject and to genres

--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -1,19 +1,19 @@
 <template>
-        <article class="movie-list_info" v-for="movieObject in fetchedMovies">
+        <article class="movie-list_info" v-for="movieObject in fetchedMovies" :key="movieObject.id">
             <div class="movie-art">
-                <img :src="movieObject.poster" alt="movie" class="movie-art_img">
+                <img :src="movieObject.poster" alt="" class="movie-art_img">
                 <div class="movie-art_controls">
-                    <button class="movie-art_controls-btn">
+                    <button class="movie-art_controls-btn" title="Add to Watch Later">
                         <img src="../assets/watchlater.svg" alt="" class="control-btn_icon">
                     </button>
-                    <button class="movie-art_controls-btn">
+                    <button class="movie-art_controls-btn" title="Add to Cart">
                         <img src="../assets/plus-circle.svg" alt="" class="control-btn_icon">
                     </button>
                 </div>
             </div>
             <div class="movie-details">
                 <h1 class="movie-details_title">{{ movieObject.title }}</h1>
-                <h2 class="movie-details_director">{{ movieObject.director }}</h2>
+                <h2 class="movie-details_director">{{ movieObject.status }}</h2>
                 <h3 class="movie-details_date">{{ movieObject.release }}</h3>
                 <ul class="movie-details_genres">
                     <li class="movie-details_genres-item" v-for="genre in movieObject.genres" :key="genre.name">{{ genre.name }}</li>
